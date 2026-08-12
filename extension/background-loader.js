1f8a915ef248f21d41c4af193152e9982e274aab
thiennv19215 <114642828+thiennv19215@users.noreply.github.com>
2026-08-11 20:20:33 -0700

    Load browser-owned transport after bridge setup

--- extension/background-loader.js
+++ extension/background-loader.js
@@ -1,1 +1,1 @@
-importScripts("config.js", "offscreen-init.js", "background.js", "session-bridge.js");
+importScripts("config.js", "offscreen-init.js", "background.js", "session-bridge.js", "browser-transport.js");
